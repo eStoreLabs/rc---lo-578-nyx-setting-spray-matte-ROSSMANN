95e8b825bb957d7dc362282d62afa362ece88ffa
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2025-05-16 09:37:29 +0200

    init fork rossmann

---
+++ Portals/0/rich-content/nyx/setting-spray-matte/js/script.js
@@ -0,0 +1,14 @@
+(() => {
+    const swiper = new Swiper('#es-rc #es-nyx .es-swiper', {
+        loop: true,
+        autoHeight: true,
+        pagination: {
+            el: '#es-rc .es-swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '#es-rc .es-swiper-button-next',
+            prevEl: '#es-rc .es-swiper-button-prev'
+        }
+    })
+})()
